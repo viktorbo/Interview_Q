045a726c0e669cbddcd27bcecf54b5aa6f37ee10
viktorbo <borisoof13@yandex.ru>
2021-02-28 01:02:20 +0300

    Some class names have been refactored

--- pages/checkme_helper.py
+++ pages/checkme_helper.py
@@ -2,7 +2,7 @@ from pages.checkme import Checkme
 from selenium.webdriver.common.by import By
 
 
-class Page_Locators:
+class PageLocators:
     # PARTS OF TABLE
     TABLE_HEADER_XPATH_LOCATOR = (By.XPATH, "//table[@id='tbl']/thead")
     TABLE_CONTENT_XPATH_LOCATOR = (By.XPATH, "//table[@id='tbl']/tbody")
@@ -24,51 +24,51 @@ class Page_Locators:
     INPUT_PRICE_FIELD_ID_LOCATOR = (By.ID, "price")
 
 
-class Checkme_Helper(Checkme):
+class CheckmeHelper(Checkme):
 
     # HEADER
     def click_the_names_header(self):
-        return self.find_element(locator=Page_Locators.NAME_HEADER_XPATH_LOCATOR, time=2).click()
+        return self.find_element(locator=PageLocators.NAME_HEADER_XPATH_LOCATOR, time=2).click()
 
     def click_the_counts_header(self):
-        return self.find_element(locator=Page_Locators.COUNT_HEADER_XPATH_LOCATOR, time=2).click()
+        return self.find_element(locator=PageLocators.COUNT_HEADER_XPATH_LOCATOR, time=2).click()
 
     def click_the_prices_header(self):
-        return self.find_element(locator=Page_Locators.PRICE_HEADER_XPATH_LOCATOR, time=2).click()
+        return self.find_element(locator=PageLocators.PRICE_HEADER_XPATH_LOCATOR, time=2).click()
 
     def click_the_actions_header(self):
-        return self.find_element(locator=Page_Locators.ACTION_HEADER_XPATH_LOCATOR, time=2).click()
+        return self.find_element(locator=PageLocators.ACTION_HEADER_XPATH_LOCATOR, time=2).click()
 
     # BUTTONS
     def click_the_open_button(self):
-        return self.find_element(locator=Page_Locators.OPEN_BUTTON_ID_LOCATOR, time=2).click()
+        return self.find_element(locator=PageLocators.OPEN_BUTTON_ID_LOCATOR, time=2).click()
 
     def click_the_discard_button(self):
-        return self.find_element(locator=Page_Locators.DISCARD_BUTTON_XPATH_LOCATOR, time=2).click()
+        return self.find_element(locator=PageLocators.DISCARD_BUTTON_XPATH_LOCATOR, time=2).click()
 
     def click_the_add_button(self):
-        return self.find_element(locator=Page_Locators.ADD_BUTTON_ID_LOCATOR, time=2).click()
+        return self.find_element(locator=PageLocators.ADD_BUTTON_ID_LOCATOR, time=2).click()
 
     def click_the_delete_record(self, n):
-        return self.find_element(locator=(By.XPATH, f"{Page_Locators.TABLE_CONTENT_XPATH_LOCATOR[1]}/tr[{n}]/td/a[@class='delete']"), time=2).click()
+        return self.find_element(locator=(By.XPATH, f"{PageLocators.TABLE_CONTENT_XPATH_LOCATOR[1]}/tr[{n}]/td/a[@class='delete']"), time=2).click()
 
     # FIELDS
     def enter_name(self, name=None):
-        name_field = self.find_element(locator=Page_Locators.INPUT_NAME_FIELD_ID_LOCATOR, time=2)
+        name_field = self.find_element(locator=PageLocators.INPUT_NAME_FIELD_ID_LOCATOR, time=2)
         if name:
             name_field.click()
             name_field.send_keys(name)
         return name_field
 
     def enter_count(self, count=None):
-        count_field = self.find_element(locator=Page_Locators.INPUT_COUNT_FIELD_ID_LOCATOR, time=2)
+        count_field = self.find_element(locator=PageLocators.INPUT_COUNT_FIELD_ID_LOCATOR, time=2)
         if count:
             count_field.click()
             count_field.send_keys(count)
         return count_field
 
     def enter_price(self, price=None):
-        price_field = self.find_element(locator=Page_Locators.INPUT_PRICE_FIELD_ID_LOCATOR, time=2)
+        price_field = self.find_element(locator=PageLocators.INPUT_PRICE_FIELD_ID_LOCATOR, time=2)
         if price:
             price_field.click()
             price_field.send_keys(price)
