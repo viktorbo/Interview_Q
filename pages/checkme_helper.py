d741403c2ad3b4403b9ac43d206895b629b0d92d
viktorbo <borisoof13@yandex.ru>
2021-02-28 01:49:04 +0300

    Added methods for table parsing on CheckMe

--- pages/checkme_helper.py
+++ pages/checkme_helper.py
@@ -79,4 +79,43 @@ class CheckmeHelper(Checkme):
         self.enter_count(count)
         self.enter_price(price)
 
+    # HEADER PARSING
+    def get_names_header(self):
+        return self.find_element(locator=PageLocators.NAME_HEADER_XPATH_LOCATOR, time=2).text
+
+    def get_counts_header(self):
+        return self.find_element(locator=PageLocators.COUNT_HEADER_XPATH_LOCATOR, time=2).text
+
+    def get_prices_header(self):
+        return self.find_element(locator=PageLocators.PRICE_HEADER_XPATH_LOCATOR, time=2).text
+
+    def get_actions_header(self):
+        return self.find_element(locator=PageLocators.ACTION_HEADER_XPATH_LOCATOR, time=2).text
+
+    def parse_table_header(self):
+        return self.get_names_header(), self.get_counts_header(), self.get_prices_header(), self.get_actions_header()
+
+    # CONTENT PARSER
+    def get_name(self, n):
+        name_xpath_locator = f"{PageLocators.TABLE_CONTENT_XPATH_LOCATOR[1]}/tr[{n}]/td[1]"
+        return self.find_element(locator=(By.XPATH, name_xpath_locator), time=1).text
+
+    def get_count(self, n):
+        count_xpath_locator = f"{PageLocators.TABLE_CONTENT_XPATH_LOCATOR[1]}/tr[{n}]/td[2]"
+        return self.find_element(locator=(By.XPATH, count_xpath_locator), time=1).text
+
+    def get_price(self, n):
+        price_xpath_locator = f"{PageLocators.TABLE_CONTENT_XPATH_LOCATOR[1]}/tr[{n}]/td[3]"
+        return self.find_element(locator=(By.XPATH, price_xpath_locator), time=1).text
+
+    def get_actions(self, n):
+        actions_xpath_locator = f"{PageLocators.TABLE_CONTENT_XPATH_LOCATOR[1]}/tr[{n}]/td[4]"
+        return tuple([action.text for action in self.find_elements(locator=(By.XPATH, actions_xpath_locator), time=1)])
+
+    def get_record(self, n):
+        return self.get_name(n), self.get_count(n), self.get_price(n), self.get_actions(n)
+
+    def parse_table_content(self):
+        content_xpath_locator = f"{PageLocators.TABLE_CONTENT_XPATH_LOCATOR[1]}/tr"
+        return [self.get_record(i+1) for i in range(len(self.find_elements(locator=(By.XPATH, content_xpath_locator))))]
 
